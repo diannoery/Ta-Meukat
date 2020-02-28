60a756870b213e4ba7163d877bd52a20abaeb221
diannoery <diannoery29@gmail.com>
2020-02-28 13:03:28 +0700

    upload projek

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShyna />
+    <HeroSection />
+    <ProductSection />
+    <InstaShyana />
+    <PatnerShyna />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShyna from "@/components/HeaderShyna.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import ProductSection from "@/components/ProductSection.vue";
+import InstaShyana from "@/components/InstaShyana.vue";
+import PatnerShyna from "@/components/PatnerShyna.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShyna,
+    HeroSection,
+    ProductSection,
+    InstaShyana,
+    PatnerShyna,
+    Footer
+  }
+};
+</script>
